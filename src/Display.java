668ad6a2f275ad5a352201f53b6c7a474a5b928a
Jerry Yang <z295yang@edu.uwaterloo.ca>
2017-01-26 20:42:28 -0500

    Version 0.0.0
    - Shows entire gameboard
    - Added Square class
    - Added MouseListener
    - Squares are now outlined when moused over

--- src/Display.java
+++ src/Display.java
@@ -1,35 +1,53 @@
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
-public class Display extends JFrame
+public class Display extends JFrame implements MouseListener, MouseMotionListener
 {
-	JPanel screen, draw, topics, score;
-	Image top;
+	private JPanel screen, draw, topics, score;
+	private Image top;
+	private ArrayList<Square> questions;
+	private Square selected;
 	public Display ()
 	{
 		screen = new JPanel(new BorderLayout());
-		draw = new DrawArea (900, 500);
-		topics = new TopBar (900, 100);
+		draw = new DrawArea (780, 500);
+		draw.addMouseListener(this);
+		draw.addMouseMotionListener(this);
+		topics = new TopBar (780, 100);
 		score = new JPanel (); 
-		
+		selected = new Square (-1, -1);
+	
 		screen.add(draw, "Center");
 		screen.add(topics, "North");
-		screen.add(score, "East");
+		screen.add(score, "South");
 		top = loadImage ("categories");
+		
+		// Add questions
+		questions = new ArrayList<Square>();
+		for (int c = 1; c <= 5; c++)
+		{
+			for (int d = 1; d <= 5; d++)
+			{
+				questions.add(new Square (c, d));
+			}
+		}
 		 
 		setContentPane(screen);
 		setTitle ("Song Jeopardy");
-		setSize (1000, 600);
+		setSize (780, 700);
 		setResizable (false);
 		setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo (null);  
@@ -60,7 +78,7 @@ public class Display extends JFrame
 	
 		public void paintComponent (Graphics g)
 		{
-			g.drawImage(top, 0, 0, 900, 100, null);
+			g.drawImage(top, 0, 0, 780, 100, null);
 		}
     }
 	
@@ -74,6 +92,85 @@ public class Display extends JFrame
 	
 		public void paintComponent (Graphics g)
 		{
+			for (int c = 0; c < questions.size(); c++)
+				questions.get(c).show(g);
 		}
     }
+	
+	public Square findSquare (int x, int y)
+	{
+		int row, col;
+		if (inBounds(x, y))
+		{
+			row = y/100 + 1;
+			col = x/158 + 1;
+		}
+		else
+		{
+			row = 1;
+			col = 1;
+		}
+		return questions.get((row-1)*5 + (col-1));
+	}
+	
+	public boolean inBounds (int x, int y)
+	{
+		return ((x >= 0 && x <= 780) && (y >= 0 && y <= 500));
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e)
+	{
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) 
+	{
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) 
+	{
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) 
+	{
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) 
+	{
+		int x = e.getX();
+		int y = e.getY();
+		if (!(findSquare(x,y).equals(selected)))
+		{
+			if (selected.getRow() != -1)
+				questions.get((selected.getRow()-1)*5 + (selected.getCol()-1)).unmouseOver();;
+			
+			if (inBounds(x,y))
+			{
+				selected = findSquare(x,y);
+				findSquare(x, y).mouseOver();
+			}
+		}
+		repaint();
+	}
 }
---
+++ src/Square.java
@@ -0,0 +1,89 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Square 
+{
+	public final int DISP_LENGTH = 156;
+	public final int DISP_HEIGHT = 100;
+	public final int GRID_LENGTH = 100;
+	public final int GRID_HEIGHT = 64;
+	private int value, row, col;
+	private boolean enabled, moused;
+	private Image scores = loadImage("scores");
+	
+	public Square (int row, int col)
+	{
+		this.row = row;
+		this.col = col;
+		value = row*100;
+		enabled = true;
+		moused = false;
+	}
+	
+	public Image loadImage (String url)
+	{
+		Image temp = null;
+		try
+	    {
+			temp = ImageIO.read (new File ("Images/" + url + ".png")); // load file into Image object     
+	    }
+	    catch (IOException e) // Catch exception if image does not exist
+	    {
+	    }
+		return temp;
+	}
+	
+	public int getRow ()
+	{
+		return row;
+	}
+	
+	public int getCol ()
+	{
+		return col;
+	}
+	
+	public int getVal ()
+	{
+		return value;
+	}
+	
+	public boolean getEnabled ()
+	{
+		return enabled;
+	}
+	
+	public void disable ()
+	{
+		enabled = false;
+	}
+	
+	public void mouseOver ()
+	{
+		if (enabled)
+			moused = true;
+	}
+	
+	public void unmouseOver ()
+	{
+		moused = false;
+	}
+	
+	public boolean equals (Square other)
+	{
+		return (row == other.getRow() && col == other.getCol());
+	}
+	
+	public void show (Graphics g)
+	{
+		g.drawImage(scores, (col-1)*DISP_LENGTH, (row-1)*DISP_HEIGHT, col*DISP_LENGTH, row*DISP_HEIGHT,
+				0, (row-1)*GRID_HEIGHT, GRID_LENGTH, row*GRID_HEIGHT, null);
+		if (moused)
+			g.drawRect((col-1)*DISP_LENGTH, (row-1)*DISP_HEIGHT, DISP_LENGTH-1, DISP_HEIGHT-1);
+	}
+	
+}
