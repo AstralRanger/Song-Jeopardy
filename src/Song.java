305b4fb9f8774aa0602f8bc5ac2a8b49243b597a
Jerry Yang <z295yang@edu.uwaterloo.ca>
2017-01-28 23:04:47 -0500

    Version 0.0.2
    - Added Song class

---
+++ src/Song.java
@@ -0,0 +1,14 @@
+
+public class Song 
+{
+	private String name, url;
+	int year, points;
+	
+	public Song (String name, String url, int year, int points)
+	{
+		this.name = name;
+		this.url = url;
+		this.year = year;
+		this.points = points;
+	}
+}
